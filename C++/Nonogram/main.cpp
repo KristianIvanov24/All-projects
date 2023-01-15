a7120f824f127b91f96eef5a1f89b6edf65f6794
KristianIvanov24 <105101729+KristianIvanov24@users.noreply.github.com>
2023-01-15 20:50:11 +0200

    Update main.cpp

--- C++/Nonogram/main.cpp
+++ C++/Nonogram/main.cpp
@@ -23,13 +23,13 @@ bool validateData(){
     char fileUsername[10], filePassword[10];
     while (file >> fileUsername >> filePassword >> level) {
         bool usernameMatch = true, passwordMatch = true;
-        for (int i = 0; i < strlen(username); i++) {
+        for (int i = 0; fileUsername[i] != '\0'; i++) {
             if (username[i] != fileUsername[i]) {
                 usernameMatch = false;
                 break;
             }
         }
-        for (int i = 0; i < strlen(password); i++) {
+        for (int i = 0; i < filePassword[i] != '\0'; i++) {
             if (password[i] != filePassword[i]) {
                 passwordMatch = false;
                 break;
@@ -37,7 +37,7 @@ bool validateData(){
         }
         if (usernameMatch && passwordMatch) {
             file.close();
-            return true;
+            return true;    
         }
     }
 
@@ -64,6 +64,73 @@ bool userReg(){
     return true;
 }
 
+void countRows(char** solved_matrix, int** &rows){
+    //int** rows = new int*[level*5];
+    int count = 0;
+    for(int i = 0; i < level*5; i++){
+        for(int j = 0; j < level*5; j++){
+            if(solved_matrix[i][j] == '#'){
+                count++;
+                rows[i][j] = 'O';
+            }
+            else if(solved_matrix[i][j] == 'O' && count != 0){
+                rows[i][j] = count;
+                count = 0;
+            }
+            else rows[i][j] = 'O';
+        }
+    }
+
+    //return rows;
+}
+
+void countColumns(char** solved_matrix, int** &columns){
+    //int** colums = new int*[level*5];
+    int count = 0;
+    for(int i = 0; i < level*5; i++){
+        for(int j = 0; j < level*5; j++){
+            if(solved_matrix[j][i] == '#'){
+                count++;
+                columns[j][i] = 'O';
+            }
+            else if(solved_matrix[i][j] == 'O' && count != 0){
+                columns[j][i] = count;
+                count = 0;
+            }
+            else columns[j][i] = 'O';
+        }
+    }
+
+    //return colums;
+}
+
+void printMatrix(char** player_matrix, int size, int** rows, int** columns){
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            if(columns[i][j] != 0)
+                std::cout << columns[i][j];
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++){
+            if(rows[i][j] != 0)
+                std::cout << rows[i][j];
+        }
+        for (int j = 0; j < size; j++) {
+            std::cout <<player_matrix[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
+
+bool compare(char** player_matrix, char** solved_matrix){}
+
+void fillChunk(char** &player_matrix, char* cmd){}
+
+void save(char** matrix){}
+
 void loadGame(){}
 
 void playGame() {
@@ -85,33 +152,39 @@ void playGame() {
         return;
     }
 
-    //std::cout << "U gud?" << std::endl;
-
     int size = level * 5;
-    //std::cout << size << std::endl;
-    char solved_matrix[size][size], player_matrix[size][size];
-    
-    //for (int i = 0; i < size; i++) {
-    //    for (int j = 0; j < size; j++) {
-    //        file >> solved_matrix[i][j];
-    //        //player_matrix[i][j] = 'O';
-    //    }
-    //}
-    //file.close();
-
+    char** solved_matrix = new char*[size];
+    char** player_matrix = new char*[size];
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
+            file >> solved_matrix[i][j];
             player_matrix[i][j] = 'O';
         }
     }
+    file.close();
 
-    // print out the matrix
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            std::cout <<player_matrix[i][j];
+    int** rows = new int*[size];
+    countRows(solved_matrix, rows);
+
+    int** columns = new int*[size];
+    countColumns(solved_matrix, columns);
+
+    char cmd[3];
+    while(!compare(player_matrix, solved_matrix)){
+        printMatrix(player_matrix, size, rows, columns);
+        std::cout << "Choose coordinates for chunk to be filled in the given format 'number1' 'number2'. If you wish to exit type 'e'." << std::endl;
+        std::cin >> cmd;
+
+        if(cmd == "e"){
+            save(player_matrix);
+            return;
         }
-        std::cout << std::endl;
+
+        else fillChunk(player_matrix, cmd);
     }
+
+    std::cout << "Congratulations! You solved level " << level << std::endl;
+    level++;
 }
 
 void NonogramGame() {
@@ -131,11 +204,14 @@ void NonogramGame() {
 
     if (cmd == 'l') {
         bool check = validateData();
+        
         while (!check) {
+            std::cout << check << std::endl;
             std::cout << "Wrong credentials!" << std::endl;
             check = validateData();
+            std::cout << check << std::endl;
         }
-
+        std::cout << "Login successful!" << std::endl;
         std::cout << "If you want to continue your last game at level " << level << " type 'c' or if you want new game type 'n'" << std::endl;
         std::cout << '>';
         std::cin >> cmd;
@@ -152,6 +228,17 @@ void NonogramGame() {
         else if (cmd == 'n')
             playGame();
         else return;
+
+        std::cout << "If you wish to play another game, please select between level 1 and level " << level << "or type 'e' to exit." << std::endl;
+        std::cin >> cmd;
+        if (cmd == 'e')
+            return;
+        else if((cmd + '0') == level)
+            playGame();
+        else{
+            level = cmd + '0';
+            loadGame();
+        }
     }
 
     else if (cmd == 'r') {
