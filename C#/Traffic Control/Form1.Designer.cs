91abf93d033f17e6150a9cdffd59d81a147039c6
KristianIvanov24 <105101729+KristianIvanov24@users.noreply.github.com>
2023-09-06 18:57:20 +0300

    Add files via upload

---
+++ C#/Traffic Control/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Контрол_по_автотранспорта
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNomer = new System.Windows.Forms.TextBox();
+            this.txtShasi = new System.Windows.Forms.TextBox();
+            this.txtMarka = new System.Windows.Forms.TextBox();
+            this.txtImena = new System.Windows.Forms.TextBox();
+            this.txtGodina = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.chStop = new System.Windows.Forms.CheckBox();
+            this.chAll = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chDanuk = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(395, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Данни за автомобила";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(138, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(331, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Регистрационен номер:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(138, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(225, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Номер на шаси:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(138, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Марка:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(138, 343);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(375, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Три имена на собственика:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(138, 404);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(348, 32);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Година на производство:";
+            // 
+            // txtNomer
+            // 
+            this.txtNomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtNomer.Location = new System.Drawing.Point(475, 157);
+            this.txtNomer.MaxLength = 6;
+            this.txtNomer.Name = "txtNomer";
+            this.txtNomer.Size = new System.Drawing.Size(415, 38);
+            this.txtNomer.TabIndex = 7;
+            // 
+            // txtShasi
+            // 
+            this.txtShasi.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtShasi.Location = new System.Drawing.Point(369, 218);
+            this.txtShasi.MaxLength = 10;
+            this.txtShasi.Name = "txtShasi";
+            this.txtShasi.Size = new System.Drawing.Size(415, 38);
+            this.txtShasi.TabIndex = 8;
+            // 
+            // txtMarka
+            // 
+            this.txtMarka.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtMarka.Location = new System.Drawing.Point(252, 276);
+            this.txtMarka.Name = "txtMarka";
+            this.txtMarka.Size = new System.Drawing.Size(415, 38);
+            this.txtMarka.TabIndex = 9;
+            // 
+            // txtImena
+            // 
+            this.txtImena.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtImena.Location = new System.Drawing.Point(519, 337);
+            this.txtImena.Name = "txtImena";
+            this.txtImena.Size = new System.Drawing.Size(415, 38);
+            this.txtImena.TabIndex = 10;
+            // 
+            // txtGodina
+            // 
+            this.txtGodina.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtGodina.Location = new System.Drawing.Point(492, 398);
+            this.txtGodina.MaxLength = 4;
+            this.txtGodina.Name = "txtGodina";
+            this.txtGodina.Size = new System.Drawing.Size(415, 38);
+            this.txtGodina.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Beige;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(401, 480);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 83);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Добави автомобил";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(1159, 77);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(713, 672);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // chStop
+            // 
+            this.chStop.AutoSize = true;
+            this.chStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chStop.Location = new System.Drawing.Point(1559, 843);
+            this.chStop.Name = "chStop";
+            this.chStop.Size = new System.Drawing.Size(311, 36);
+            this.chStop.TabIndex = 14;
+            this.chStop.Text = "Спрени от движение";
+            this.chStop.UseVisualStyleBackColor = true;
+            // 
+            // chAll
+            // 
+            this.chAll.AutoSize = true;
+            this.chAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chAll.Location = new System.Drawing.Point(1559, 788);
+            this.chAll.Name = "chAll";
+            this.chAll.Size = new System.Drawing.Size(298, 36);
+            this.chAll.TabIndex = 15;
+            this.chAll.Text = "Всички автомобили";
+            this.chAll.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Beige;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(1159, 788);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(266, 75);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Справка";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chDanuk
+            // 
+            this.chDanuk.AutoSize = true;
+            this.chDanuk.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chDanuk.Location = new System.Drawing.Point(1559, 900);
+            this.chDanuk.Name = "chDanuk";
+            this.chDanuk.Size = new System.Drawing.Size(122, 36);
+            this.chDanuk.TabIndex = 17;
+            this.chDanuk.Text = "Данък";
+            this.chDanuk.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Beige;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(1159, 885);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(266, 75);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Изчисти";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Контрол_по_автотранспорта.Properties.Resources.imageedit_3_5426691557;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(46, 607);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(900, 400);
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(1924, 972);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.chDanuk);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chAll);
+            this.Controls.Add(this.chStop);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtGodina);
+            this.Controls.Add(this.txtImena);
+            this.Controls.Add(this.txtMarka);
+            this.Controls.Add(this.txtShasi);
+            this.Controls.Add(this.txtNomer);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Контрол по автотранспорта";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNomer;
+        private System.Windows.Forms.TextBox txtShasi;
+        private System.Windows.Forms.TextBox txtMarka;
+        private System.Windows.Forms.TextBox txtImena;
+        private System.Windows.Forms.TextBox txtGodina;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox chStop;
+        private System.Windows.Forms.CheckBox chAll;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox chDanuk;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ C#/Traffic Control/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Контрол_по_автотранспорта
+{
+    public partial class Form1 : Form
+    {      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dvijenie;      
+            string danuk;
+            string godina = txtGodina.Text;
+            int godinaa = int.Parse(godina);
+            if (string.IsNullOrWhiteSpace(txtNomer.Text) || string.IsNullOrWhiteSpace(txtShasi.Text) || string.IsNullOrWhiteSpace(txtMarka.Text) || string.IsNullOrWhiteSpace(txtImena.Text) || string.IsNullOrWhiteSpace(txtGodina.Text))
+            {
+                MessageBox.Show("Имате непопълнено поле!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                if(!(char.IsLetter(txtNomer.Text[0]) && char.IsLetter(txtNomer.Text[1]) && char.IsDigit(txtNomer.Text[2]) && char.IsDigit(txtNomer.Text[3]) && char.IsDigit(txtNomer.Text[4]) && char.IsDigit(txtNomer.Text[5])))
+                {
+                    MessageBox.Show("Грешен регистрационен номер!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                if (!txtShasi.Text.All(Char.IsDigit))
+                {
+                    MessageBox.Show("Грешно въведен номер на шаси!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                else
+                    if(txtShasi.Text.Length!=10)
+                    {
+                        MessageBox.Show("Грешно въведен номер на шаси!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                if (!txtGodina.Text.All(Char.IsDigit))
+                {
+                    MessageBox.Show("Грешно въведена година на производство!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+            if (godinaa >= 1995)
+            {
+                dvijenie = "Автомобилът е в движение!";
+                danuk = "Данък-" + (50 + (2020 - godinaa) + "лв.");
+            }
+            else
+            {
+                dvijenie = "Автомобилът е спрян от движение!";
+                danuk = "Данък- 50лв.";
+            }
+            StreamWriter Myfile = new StreamWriter("Автомобили.txt", true, Encoding.GetEncoding(1251));
+            Myfile.WriteLine(txtNomer.Text + ", " + txtShasi.Text + ", " + txtMarka.Text + ", " + txtImena.Text + ", " + txtGodina.Text + ", " + dvijenie);
+            StreamWriter File = new StreamWriter("Данък.txt", true, Encoding.GetEncoding(1251));
+            File.WriteLine(txtImena.Text + ", " + txtNomer.Text + ", " + danuk);
+            txtNomer.Clear();
+            txtShasi.Clear();
+            txtMarka.Clear();
+            txtImena.Clear();
+            txtGodina.Clear();
+            Myfile.Close();
+            File.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            StreamReader myFile = new StreamReader("Автомобили.txt", Encoding.GetEncoding(1251));
+            StreamReader File = new StreamReader("Данък.txt", Encoding.GetEncoding(1251));
+            string fileLine = "";
+            string fileLine2 = "";
+            while (fileLine != null && fileLine2 != null)
+            {
+                fileLine = myFile.ReadLine();
+                fileLine2 = File.ReadLine();
+                if (fileLine != null && chAll.Checked)
+                {
+                    richTextBox1.Text += fileLine + '\n';
+                }
+                if (fileLine != null && chStop.Checked && fileLine.IndexOf("Автомобилът е спрян от движение!") > -1)
+                {
+                    richTextBox1.Text += fileLine + '\n';
+                }
+                if (fileLine2 != null && chDanuk.Checked)
+                {
+                    richTextBox1.Text += fileLine2 + '\n';
+                }
+            }
+            myFile.Close();
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+    }
+ }
